0958281908a0c0abf7917a61c2d0fc453a8c6e9d
Japerrier <jaiperrier@gmail.com>
2018-04-27 00:34:20 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+    require './includes/header.php';
+?>
+
+</section>
+
+<article>
+    <p>
+        Welcome information for the website goes here.
+    </p>
+</article>
+</body>
+
+<?php
+    include './includes/footer.php';
+?>

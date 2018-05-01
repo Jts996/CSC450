c5799669e43bdec09f2e4758ee7b5c5f58f02306
Japerrier <jaiperrier@gmail.com>
2018-05-01 00:24:39 -0400

    login/register
    login.php and register.php updated. Still needs testing and minor adjustments. Sessions are entirely untested.

--- login.php
+++ login.php
@@ -1,16 +1,91 @@
-<?php
-    require './includes/header.php';
+<?php session_start();
+
+$errors = [];
+$missing = [];
+//check for login attempt
+if (isset($_POST['login'])) {
+    //process login attempt
+    $expected = ['email', 'pwd'];
+    $required = ['email', 'pwd'];
+
+    $email=trim(filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL));
+    if (empty($email))
+        $missing[]='email';
+    $pwd=trim(filter_input(INPUT_POST,'pwd', FILTER_SANITIZE_STRING));
+    if (empty($pwd))
+        $missing[]='pwd';
+
+    //connect to the database
+    if (!$missing && !$errors) {
+        try {
+            require_once('../../pdo_config.php');
+            //check database for matching email address
+            $sql = 'SELECT email, password, isAdmin FROM users WHERE email = :email';
+            $stmt = $conn->prepare($sql);
+            $stmt->execute([':email' => $email]);
+            $rows = $stmt->rowCount();
+            //get hashed password and admin status if a user was returned
+            if ($rows != 0) {
+                $row = $stmt->fetch();
+                $hash = $row[1];
+                $isAdmin = $row[2];
+                $_SESSION['isAdmin'] = $isAdmin;
+                $_SESSION['signedIn'] = $signedIN;
+                //verify password and redirect to admin or index page
+                if (password_verify($pwd, $hash)){
+                    if ($isAdmin == 1)
+                        header('Location: admin.php');
+                    else
+                        header('Location: index.php');
+                }
+                //display message if password is not correct
+                else
+                    echo '<h3>Your password is incorrect. Please try again.</h3>';
+            }
+            //display message if email not found in database
+            else
+                echo '<h3>No accounts found. Please try again or register.</h3>';
+        }
+        catch (PDOException $e) {
+            echo $e->getMessage();
+            echo '<h3>We are unable to process your login attempt at this time. Please try again later.</h3>';
+            include 'includes/footer.php';
+            exit;
+        }
+    }
+}
+require './includes/header.php';
 ?>
 
-</section>
+    <form method="post" action="login.php">
+        <fieldset>
+            <legend>
+                <h1>Log In</h1>
+            </legend>
+            <h3>
+                <label for="email">Email:
+                    <?php if ($missing && in_array('email', $missing)) { ?>
+                        <span class="warning">Please enter a valid email address</span>
+                    <?php } ?>
+                </label><br>
+                <input type="email" name="email" id="email"
+                    <?php if ($missing || $errors) {
+                        echo 'value="' . htmlentities($email) . '"';
+                    } ?>> <br>
 
-<article>
-    <p>
-        This will be a form for users to sign in to the website.
-    </p>
-</article>
-</body>
+                <label for="pwd">Password:
+                    <?php if($missing && in_array('pwd', $missing)) { ?>
+                        <span class="warning">Please enter your password</span>
+                    <?php } ?>
+                </label><br>
+                <input type="password" name="pwd" id="pwd"><br><br>
+
+                <input type="submit" value="Log In" name="login"
+
+            </h3>
+        </fieldset>
+    </form>
 
 <?php
-    include './includes/footer.php';
+include'includes/footer.php'
 ?>
--- register.php
+++ register.php
@@ -1,17 +1,139 @@
-<?php
-    require './includes/header.php';
+<?php session_start();
+
+require './includes/header.php';
+$errors = [];
+$missing = [];
+//check if the form has been sumbitted
+if (isset($_POST['submit'])) {
+    //process form
+
+    $expected = ['firstname', 'lastname', 'email', 'phone', 'pwd1', 'pwd2'];
+    $required = ['firstname', 'lastname', 'email', 'phone', 'pwd1', 'pwd2'];
+
+    $firstname=trim(filter_input(INPUT_POST, 'firstname', FILTER_SANITIZE_STRING));
+    if (empty($firstname))
+        $missing[]='firstname';
+
+    $lastname=trim(filter_input(INPUT_POST, 'lastname', FILTER_SANITIZE_STRING));
+    if (empty($lastname))
+        $missing[]='lastname';
+
+    $email=trim(filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL));
+    if (empty($email))
+        $missing[]='email';
+
+    $phone=trim(filter_input(INPUT_POST, 'phone', FILTER_SANITIZE_NUMBER_INT));
+    if (empty($phone))
+        $missing[]='phone';
+
+    $pwd1=trim(filter_input(INPUT_POST, 'pwd1', FILTER_SANITIZE_STRING));
+    $pwd2=trim(filter_input(INPUT_POST, 'pwd2', FILTER_SANITIZE_STRING));
+    if (empty($pwd1) || empty($pwd2))
+        $missing[]='pwd1';
+    elseif ($pwd1 != $pwd2)
+        $errors[]='pwd1';
+    else ($pwd = password_hash("$pwd1", PASSWORD_DEFAULT));
+
+    if (!$missing && !$errors) {
+        try {
+            require_once('../../pdo_config.php');
+            $sql = "INSERT into users (email, firstName, lastName, phone, password) VALUES ('$email', '$firstname', '$lastname', '$phone', '$pwd')";
+            if($conn->query($sql))
+                echo '<h3>Thank you for registering</h3>';
+            else
+                echo '<h3>We were unable to process your registration at this time</h3>';
+            include 'includes/footer.php';
+            exit;
+        }
+        catch (PDOException $e) {
+            echo $e->getMessage();
+            echo '<h3>We were unable to process your registration at this time</h3>';
+            include 'includes/footer.php';
+            exit;
+        }
+    }
+}
 ?>
 
-</section>
+<form method="post" action="register.php">
 
-<article>
-    <p>
-        This will be a form for users to register to our website.<br>
-        This information will be stored in the database and can be used to sign in.
-    </p>
-</article>
-</body>
+    <fieldset>
+        <legend>
+            <h1>Create an Account</h1>
+        </legend>
+        <?php
+        if ($missing || $errors) { ?>
+            <p class="warning">Please fix the item(s) indicated.</p>
+        <?php } ?>
+        <h3>
+            <label for="firstname">First Name:
+                <?php if ($missing && in_array('firstname', $missing)) { ?>
+                    <span class="warning">Please enter your first name</span>
+                <?php } ?>
+            </label><br>
+            <input type="text" name="firstname" id="firstname"
+                <?php if ($missing || $errors) {
+                    echo 'value="' . htmlentities($firstname) . '"';
+                } ?>> <br>
 
-<?php
-    include './includes/footer.php';
-?>
+            <label for="lastname">Last Name:
+                <?php if ($missing && in_array('lastname', $missing)) { ?>
+                    <span class="warning">Please enter your last name</span>
+                <?php } ?>
+            </label><br>
+            <input type="text" name="lastname" id="lastname"
+                <?php if ($missing || $errors) {
+                    echo 'value="' . htmlentities($lastname) . '"';
+                } ?>> <br>
+
+            <label for="email">Email:
+                <?php if ($missing && in_array('email', $missing)) { ?>
+                    <span class="warning">Please enter your email address</span>
+                <?php } ?>
+            </label><br>
+            <input type="email" name="email" id="email"
+                <?php if ($missing || $errors) {
+                    echo 'value="' . htmlentities($email) . '"';
+                } ?>> <br>
+
+            <label for="phone">Phone Number:
+                <?php if ($missing && in_array('phone', $missing)) { ?>
+                    <span class="warning">Please enter your phone number</span>
+                <?php } ?>
+            </label><br>
+            <input type="tel" name="phone" id="phone"
+                <?php if ($missing || $errors) {
+                    echo 'value="' . htmlentities($phone) . '"';
+                }?>> <br>
+
+            <?php if ($errors && in_array('pwd1', $errors)) { ?>
+                <span class="warning">The passwords entered do not match</span>
+            <?php } ?>
+            <label for="pwd1">Password:
+                <?php if ($missing && in_array('pwd1', $missing)) { ?>
+                    <span class="warning">Please enter a password</span>
+                <?php } ?>
+            </label><br>
+            <input type="password" name="pwd1" id="pwd1"> <br>
+
+            <?php if ($errors && in_array('pwd2', $errors)) { ?>
+                <span class="warning">The passwords entered do not match</span>
+            <?php } ?>
+            <label for="pwd2">Re-enter Password:
+                <?php if ($missing && in_array('pwd2', $missing)) { ?>
+                    <span class="warning">Please re-enter your password</span>
+                <?php } ?>
+            </label><br>
+            <input type="password" name="pwd2" id="pwd2"> <br><br>
+
+            <input type="submit" value="Submit" name="submit">
+
+        </h3>
+    </fieldset>
+
+
+
+</form>
+
+
+<?php include'includes/footer.php'; ?>

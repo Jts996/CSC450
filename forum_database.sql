a6ee490547c9e5cc04d1d82adaca706ed1f491b8
Jts996 <simpsonj948@gmail.com>
2018-05-02 02:59:53 -0400

    Finished forum database

--- forum_database.sql
+++ forum_database.sql
@@ -1,30 +1,23 @@
-DROP TABLE IF EXISTS post;
 DROP TABLE IF EXISTS reply;
 DROP TABLE IF EXISTS topic;
 
 
+
 CREATE TABLE topic (topic_id INT NOT NULL AUTO_INCREMENT,
               title VARCHAR(150) NOT NULL,
+              user_email VARCHAR(80) NOT NULL,
               date DATETIME NOT NULL,
               created_by_user_id INT NOT NULL,
               PRIMARY KEY (topic_id),
               FOREIGN KEY (created_by_user_id) REFERENCES users(user_id))ENGINE = INNODB;
 
 
-CREATE TABLE post (post_id INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE reply (reply_id INT NOT NULL AUTO_INCREMENT,
              post_content text NOT NULL,
+             user_email VARCHAR(80) NOT NULL,
              date DATETIME NOT NULL,
              posted_by_user_id INT NOT NULL,
              topic INT NOT NULL,
-             PRIMARY KEY (post_id),
+             PRIMARY KEY (reply_id),
              FOREIGN KEY (posted_by_user_id) REFERENCES users(user_id),
              FOREIGN KEY (topic) REFERENCES topic(topic_id))ENGINE = INNODB;
-
-CREATE TABLE reply (reply_id INT NOT NULL AUTO_INCREMENT,
-              reply_content TEXT NOT NULL,
-              date DATETIME NOT NULL,
-              posted_by_user_id INT NOT NULL,
-              post_id INT NOT NULL,
-              PRIMARY KEY (reply_id),
-              FOREIGN KEY (posted_by_user_id) REFERENCES users(user_id),
-              FOREIGN KEY (post_id) REFERENCES post(post_id))ENGINE = INNODB;

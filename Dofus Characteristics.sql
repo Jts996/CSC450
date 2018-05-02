918993643101c03016e9d116174eb0660b757fd8
Jts996 <simpsonj948@gmail.com>
2018-05-01 22:59:56 -0400

    Made some adjustment so it follows the relational database rules

--- Dofus Characteristics.sql
+++ Dofus Characteristics.sql
@@ -2,17 +2,30 @@ DROP TABLE IF EXISTS resistances;
 DROP TABLE IF EXISTS damages;
 DROP TABLE IF EXISTS secondary_characteristics;
 DROP TABLE IF EXISTS primary_characteristics;
+DROP TABLE IF EXISTS classes;
+DROP TABLE IF EXISTS servers;
 DROP TABLE IF EXISTS characters;
 
 
-CREATE TABLE characters (name VARCHAR(20) NOT NULL,
-                    server VARCHAR(20) NOT NULL,
-                    class VARCHAR(20) NOT NULL,
-                    level INT NOT NULL,
-                    PRIMARY KEY (name)) ENGINE = INNODB;
-
-CREATE TABLE primary_characteristics (primary_id int not null,
+CREATE TABLE characters (char_id INT NOT NULL,
                     name VARCHAR(20) NOT NULL,
+                    server INT NOT NULL,
+                    class INT NOT NULL,
+                    level INT NOT NULL,
+                    PRIMARY KEY (char_id),
+                    FOREIGN KEY(class) REFERENCES classes(class_id),
+                    FOREIGN KEY(server) REFERENCES servers(server_id)) ENGINE = INNODB;
+
+CREATE TABLE classes (class_id INT NOT NULL,
+                  name VARCHAR(20) NOT NULL,
+                  PRIMARY KEY (class_id)) ENGINE = INNODB;
+
+CREATE TABLE servers (server_id INT NOT NULL,
+                  name VARCHAR(20) NOT NULL,
+                  PRIMARY KEY (server_id)) ENGINE = INNODB;
+
+CREATE TABLE primary_characteristics (primary_id INT NOT NULL,
+                    char_id INT NOT NULL,
                     vitality INT NOT NULL,
                     wisdom INT NOT NULL,
                     strength INT NOT NULL,
@@ -21,28 +34,27 @@ CREATE TABLE primary_characteristics (primary_id int not null,
                     agility INT NOT NULL,
                     ap INT NOT NULL,
                     mp INT NOT NULL,
-                    initiative INT NOT NULL,
-                    prospecting INT NOT NULL,
-                    distance INT NOT NULL,
-                    summons INT NOT NULL,
+                    critical_hits INT NOT NULL,
                     PRIMARY KEY (primary_id),
-                    FOREIGN KEY (name) REFERENCES characters(name)) ENGINE = INNODB;
+                    FOREIGN KEY (char_id) REFERENCES characters(char_id)) ENGINE = INNODB;
 
 CREATE TABLE secondary_characteristics (secondary_id INT NOT NULL,
-                    name VARCHAR(20) NOT NULL,
+                    char_id INT NOT NULL,
                     ap_reduction INT NOT NULL,
                     ap_loss_res INT NOT NULL,
                     mp_reduction INT NOT NULL,
                     mp_loss_res INT NOT NULL,
-                    critical_hits INT NOT NULL,
+                    initiative INT NOT NULL,
+                    prospecting INT NOT NULL,
+                    summons INT NOT NULL,
                     heals INT NOT NULL,
                     catch INT NOT NULL,
                     dodge INT NOT NULL,
                     PRIMARY KEY (secondary_id),
-                    FOREIGN KEY (name) REFERENCES characters(name)) ENGINE = INNODB;
+                    FOREIGN KEY (char_id) REFERENCES characters(char_id)) ENGINE = INNODB;
 
 CREATE TABLE damages (damages_id INT NOT NULL,
-                    name VARCHAR(20) NOT NULL,
+                    char_id INT NOT NULL,
                     damage INT NOT NULL,
                     power INT NOT NULL,
                     critical_damage INT NOT NULL,
@@ -56,10 +68,10 @@ CREATE TABLE damages (damages_id INT NOT NULL,
                     trap_power INT NOT NULL,
                     pushback_damage INT NOT NULL,
                     PRIMARY KEY (damages_id),
-                    FOREIGN KEY (name) REFERENCES characters(name)) ENGINE = INNODB;
+                    FOREIGN KEY (char_id) REFERENCES characters(char_id)) ENGINE = INNODB;
 
 CREATE TABLE resistances (resist_id INT NOT NULL,
-                    name VARCHAR(20) NOT NULL,
+                    char_id INT NOT NULL,
                     neutral_res_fixed INT NOT NULL,
                     neutral_res_perc INT NOT NULL,
                     earth_res_fixed INT NOT NULL,
@@ -73,4 +85,4 @@ CREATE TABLE resistances (resist_id INT NOT NULL,
                     critical_res_fixed INT NOT NULL,
                     pushback_res_fixed INT NOT NULL,
                     PRIMARY KEY (resist_id),
-                    FOREIGN KEY (name) REFERENCES characters(name)) ENGINE = INNODB;
+                    FOREIGN KEY (char_id) REFERENCES characters(char_id)) ENGINE = INNODB;

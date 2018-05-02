9fd5153c911b6102beece303b5ac080ac4787a56
Jts996 <simpsonj948@gmail.com>
2018-05-02 02:25:33 -0400

    The implementation of the leaderboard, there is some javascript alternative syntax which needs changed as i couldnt find the php alternative

--- game_data.php
+++ game_data.php
@@ -1,14 +1,55 @@
 <?php
     require './includes/header.php';
+
+
+
+        //Connecting to the database
+        $servername = "localhost";
+        $username = "username";
+        $password = "password";
+    $database = mysql_connect()
+
 ?>
 
-</section>
+<article class="search_table">
+    <label>Username: </label>
+    <input type="text" name="username" value="<?php echo $name?>"/><br>
+    <label>Number of Results: </label>
+    <input type="text" name="numOfRes" value="<?php echo $result_limit?>"/><br>
+    <input type="submit" name="submitButton" value="submit"/>
+
+</article>
+
 
 <article>
-    <p>
-        Information about players will go here.<br>
-        This information will be pulled from the sql database and displayed in a sortable table.
-    </p>
+    <table>
+        <tr>
+            <th>Rank</th>
+            <th>Name</th>
+            <th>Score</th>
+        </tr>
+        <?php
+            $name = $_POST['username'];
+            $result_limit = $_POST['numOfRes'];
+            $submitButton = $_POST['submitButton']
+
+
+            //Query the database for the requested data
+            $result = $conn->query("SELECT username, score FROM players ORDER BY score LIMIT 10");
+            $rank = 1;
+            //Checking if $result has a new line before trying to input the data
+            if(has.next($result)){
+                //Place data in a table/leaderboard
+                while($row = $result){
+                    echo "<td>{$rank}</td>
+                          <td>{$row['user']}</td>
+                          <td>{$row['score']}</td>";
+                    //Increment rank by one for every data entry in the table
+                    $rank++;
+                }
+            }
+        ?>
+    </table>
 </article>
 </body>
 

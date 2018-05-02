4d3da1ff66ea7bf6a7488c0b6f0070a1f7c91b19
MaLevey <37633161+MaLevey@users.noreply.github.com>
2018-05-02 13:52:15 -0400

    Update About Us.php

--- About Us.php
+++ About Us.php
@@ -6,6 +6,7 @@
       About the J-Men <br>
     </h>
 
+    <!--
     <aside>
        <h2 id="textbord">
           We, the J-Men (and Mike), are four computer science majors studying at UNCW.
@@ -16,11 +17,17 @@
           We hope that you enjoy this website and support the potential features yet to come!
        </h2>
     <aside>
+    -->
         
 
     <article>
         <p>
-            Here we will have information our company and/or the game itself.
+            We, the J-Men (and Mike), are four computer science majors studying at UNCW.
+          As fellow gaming enthusiasts with an eagerness to learn new coding skills, we decided
+          to create this website for our CSC 450 class. The website is still in it's basic stages,
+          but with some attention, we would hope that it could bring some life to a game on
+          the decline in popularity. <br><br>
+          We hope that you enjoy this website and support the potential features yet to come!
         </p>
     </article>
 </body>

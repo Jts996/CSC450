805f043c2da6b283cbc593edd0a1dfe3a36c1137
Jts996 <simpsonj948@gmail.com>
2018-05-02 14:31:02 -0400

    Updated version

--- forum_database.sql
+++ forum_database.sql
@@ -1,23 +1,14 @@
-DROP TABLE IF EXISTS reply;
-DROP TABLE IF EXISTS topic;
-
-
-
-CREATE TABLE topic (topic_id INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS topic (topic_id INT NOT NULL AUTO_INCREMENT,
               title VARCHAR(150) NOT NULL,
               user_email VARCHAR(80) NOT NULL,
               date DATETIME NOT NULL,
-              created_by_user_id INT NOT NULL,
-              PRIMARY KEY (topic_id),
-              FOREIGN KEY (created_by_user_id) REFERENCES users(user_id))ENGINE = INNODB;
+              PRIMARY KEY (topic_id))ENGINE = INNODB;
 
 
-CREATE TABLE reply (reply_id INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS reply (reply_id INT NOT NULL AUTO_INCREMENT,
              post_content text NOT NULL,
              user_email VARCHAR(80) NOT NULL,
              date DATETIME NOT NULL,
-             posted_by_user_id INT NOT NULL,
              topic INT NOT NULL,
              PRIMARY KEY (reply_id),
-             FOREIGN KEY (posted_by_user_id) REFERENCES users(user_id),
              FOREIGN KEY (topic) REFERENCES topic(topic_id))ENGINE = INNODB;

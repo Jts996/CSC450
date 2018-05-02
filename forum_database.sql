373dd23a5fe1d70917a663ee3cb05d77bb887eca
Jts996 <simpsonj948@gmail.com>
2018-05-02 02:05:52 -0400

    Added topic table to organise all posts

--- forum_database.sql
+++ forum_database.sql
@@ -1,19 +1,30 @@
 DROP TABLE IF EXISTS post;
 DROP TABLE IF EXISTS reply;
+DROP TABLE IF EXISTS topic;
+
+
+CREATE TABLE topic (topic_id INT NOT NULL AUTO_INCREMENT,
+              title VARCHAR(150) NOT NULL,
+              date DATETIME NOT NULL,
+              created_by_user_id INT NOT NULL,
+              PRIMARY KEY (topic_id),
+              FOREIGN KEY (created_by_user_id) REFERENCES users(user_id))ENGINE = INNODB;
 
 
 CREATE TABLE post (post_id INT NOT NULL AUTO_INCREMENT,
              post_content text NOT NULL,
              date DATETIME NOT NULL,
-             user_id INT NOT NULL,
+             posted_by_user_id INT NOT NULL,
+             topic INT NOT NULL,
              PRIMARY KEY (post_id),
-             FOREIGN KEY (user_id) REFERENCES users(user_id))ENGINE = INNODB;
+             FOREIGN KEY (posted_by_user_id) REFERENCES users(user_id),
+             FOREIGN KEY (topic) REFERENCES topic(topic_id))ENGINE = INNODB;
 
 CREATE TABLE reply (reply_id INT NOT NULL AUTO_INCREMENT,
               reply_content TEXT NOT NULL,
               date DATETIME NOT NULL,
-              user_id INT NOT NULL,
+              posted_by_user_id INT NOT NULL,
               post_id INT NOT NULL,
               PRIMARY KEY (reply_id),
-              FOREIGN KEY (user_id) REFERENCES users(user_id),
+              FOREIGN KEY (posted_by_user_id) REFERENCES users(user_id),
               FOREIGN KEY (post_id) REFERENCES post(post_id))ENGINE = INNODB;

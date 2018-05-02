316d443404249f958c5832c32922e03008ec5ee5
Jts996 <simpsonj948@gmail.com>
2018-05-02 01:54:49 -0400

    Variable name change

--- forum_database.sql
+++ forum_database.sql
@@ -3,14 +3,14 @@ DROP TABLE IF EXISTS reply;
 
 
 CREATE TABLE post (post_id INT NOT NULL AUTO_INCREMENT,
-             subject VARCHAR(1000) NOT NULL,
+             post_content text NOT NULL,
              date DATETIME NOT NULL,
              user_id INT NOT NULL,
              PRIMARY KEY (post_id),
              FOREIGN KEY (user_id) REFERENCES users(user_id))ENGINE = INNODB;
 
 CREATE TABLE reply (reply_id INT NOT NULL AUTO_INCREMENT,
-              subject VARCHAR(1000) NOT NULL,
+              reply_content TEXT NOT NULL,
               date DATETIME NOT NULL,
               user_id INT NOT NULL,
               post_id INT NOT NULL,
